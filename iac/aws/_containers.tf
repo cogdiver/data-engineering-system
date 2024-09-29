cd16f06a29603ffc3dc3efd0d54b44bd3659d3e0
cogdiver <valentinaarenas2@gmail.com>
2024-09-29 11:32:19 -0500

    add aws ECS cluster definition

--- iac/aws/_containers.tf
+++ iac/aws/_containers.tf
@@ -1,5 +1,10 @@
 # Create the ECR repositories
-resource "aws_ecr_repository" "server_repo" {
+resource "aws_ecr_repository" "repo" {
   name         = "${var.project_name_base}_repo"
   force_delete = true
+}
+
+# Create the ECS cluster
+resource "aws_ecs_cluster" "cluster" {
+  name  = "${var.project_name_base}_cluster"
 }
