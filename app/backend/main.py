13a8058bf29f316a8a88ab04a86ae83533bfc638
cogdiver <valentinaarenas2@gmail.com>
2024-12-11 18:42:28 -0500

    add endpoint to backend

--- app/backend/main.py
+++ app/backend/main.py
@@ -8,3 +8,12 @@ def read_root():
     return {
         "message": "Welcome to Data Engineering System project"
     }
+
+rows = [
+    {'id': 1},
+    {'id': 2},
+    {'id': 3},
+]
+@app.get("/rows")
+def read_rows():
+    return rows
