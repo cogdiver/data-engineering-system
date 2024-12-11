18dba8f7b39be931fb08288a6b6b1f7bf1dd7e7a
cogdiver <valentinaarenas2@gmail.com>
2024-12-11 11:00:02 -0500

    update services

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
-# from endpoints.v1.logs import router as endpoints_v1_logs
 
 app = FastAPI()
-
-# Mount the routers of the different endpoints
-# app.include_router(endpoints_v1_logs, prefix="/v1/logs", tags=["v1/logs"])
 
 
 @app.get("/")
