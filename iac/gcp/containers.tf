999d970d5d9e8b7adf636da032a56f00805e150b
cogdiver <valentinaarenas2@gmail.com>
2024-12-12 08:41:48 -0500

    update gcp IaC

--- iac/gcp/containers.tf
+++ iac/gcp/containers.tf
@@ -1,4 +1,5 @@
 resource "google_artifact_registry_repository" "repo" {
+  depends_on = [ google_project_service.enable_apis ]
   location     = var.region # Ajusta la región según tus necesidades
   repository_id = "${var.project_name_base}-repository"
   format       = "DOCKER"
--- iac/gcp/olap.tf
+++ iac/gcp/olap.tf
@@ -1,4 +1,5 @@
 resource "google_bigquery_dataset" "my_dataset" {
+  depends_on = [ google_project_service.enable_apis ]
   dataset_id    = "${var.project_name_base}_dataset"
   location      = var.region
   # description   = "BigQuery Dataset"
